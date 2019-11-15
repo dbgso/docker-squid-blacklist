4f648c6f95651371bcbe5aab00228a7e942e768c
dbgso <s092018@gmail.com>
2019-11-15 21:14:35 +0900

    Modify: fix permission for squid user

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,12 @@ squid
 # reload configs when blacklist is modified
 inotifywait -m /etc/squid/blacklist* -e MODIFY  | xargs -i sh -c "squid -k reconfigure && echo restart" &
 
+# add permissino to squid user
+chmod -R 755 /var/log/squid
+chown -R squid:squid /var/log/squid
+chmod -R 755 /var/cache/squid
+chown -R squid:squid /var/cache/squid
+
 # wait if logs have not created
 if [ ! -e /var/log/squid/access.log ];then
     echo wait logs

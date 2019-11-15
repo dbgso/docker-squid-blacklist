b7f98ec44ad930ce42a911cd640d60d5e94efad6
osaki <osaki@cloudpack.jp>
2019-11-15 17:39:52 +0900

    Add: config for blacklist regex

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 squid
 
 # reload configs when blacklist is modified
-inotifywait -m /etc/squid/blacklist -e MODIFY  | xargs -i sh -c "squid -k reconfigure && echo restart" &
+inotifywait -m /etc/squid/blacklist* -e MODIFY  | xargs -i sh -c "squid -k reconfigure && echo restart" &
 
 # wait if logs have not created
 if [ ! -e /var/log/squid/access.log ];then
